b3174cb7a2d07ee0913c93e2fb8878bc1f21f726
Tasgo <tasgo@outlook.com>
2016-01-18 00:20:15 -0500

    Added dummy test file.

---
+++ src/test/java/org/tasgo/faronade/FaronadeTest.java
@@ -0,0 +1,19 @@
+package org.tasgo.faronade;
+
+import org.junit.Test;
+
+/**
+ * Created by Tasgo on 1/18/2016.
+ */
+public class FaronadeTest {
+
+    @Test
+    public void testRegisterMethod() throws Exception {
+
+    }
+
+    @Test
+    public void testInvoke() throws Exception {
+
+    }
+}
